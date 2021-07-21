a38cdc5e2d59b6e7bdef1ba464fc4972a20b8e94
mnoor921 <mnoor92082@hotmail.com>
2021-07-21 09:08:38 +0500

    757 remove

--- src/pages/Homepage/Navbar.jsx
+++ src/pages/Homepage/Navbar.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Navbar,Nav } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 function Navbar1() {
   return (
     <div>
       <Container className="navbar-style" sticky="top">
-        <Navbar bg="white" expand="lg"  >
-          <Link className="brand" to="/commmenu">FMC/CDU TRAINERS</Link>
+        <Navbar bg="white" expand="lg">
+          <Link className="brand" to="/commmenu">
+            FMC/CDU TRAINERS
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Link className="top-navbar" to="/commmenu"> 787/777 Comm</Link>
-              <Link to="/Project777" className="top-navbar" >777 FMC</Link>
-              <Link to="/comming" className="top-navbar" >757 CDU</Link>
-              <Link to="/project3" className="top-navbar" >787 FMC</Link>
-              <Link to="/project2" className="top-navbar" >737 FMC</Link>
-              <Link to="/A20project" className="top-navbar" >320/319 CDU</Link>
+              <Link className="top-navbar" to="/commmenu">
+                {" "}
+                787/777 Comm
+              </Link>
+              <Link to="/Project777" className="top-navbar">
+                777 FMC
+              </Link>
+              {/* <Link to="/comming" className="top-navbar" >757 CDU</Link> */}
+              <Link to="/project3" className="top-navbar">
+                787 FMC
+              </Link>
+              <Link to="/project2" className="top-navbar">
+                737 FMC
+              </Link>
+              <Link to="/A20project" className="top-navbar">
+                320/319 CDU
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </Container>
-     
     </div>
   );
 }
